1b125a66586013e77623fda02e827d7837f03004
Thaddeus Madison Jr <thaddeus.madison@gmail.com>
2017-07-01 15:16:24 -0500

    added a npm script for stopping flow status

---
+++ src/components/first-component/index.js
@@ -0,0 +1,17 @@
+// @flow
+import React from 'react';
+import * as jest from 'jest';
+
+export default class Welcome extends React.Component {
+  render() {
+    return (
+      <div>Hello world</div>
+    );
+  }
+}
+
+function square(n: number): number {
+  return n * n;
+}
+
+square("5"); // Error!
