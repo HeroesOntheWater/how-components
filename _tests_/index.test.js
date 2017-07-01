9bf7dfe02394acfe0254d021a26b7cb180e11e6d
Thaddeus Madison Jr <thaddeus.madison@gmail.com>
2017-07-01 03:56:56 -0500

    added axejs for accessibility and updated tests to use axe.js

--- _tests_/index.test.js
+++ _tests_/index.test.js
@@ -1,11 +1,27 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 import Welcome from '../src/index.js';
+import axe from 'axe-core';
 
 describe('Welcome', () => {
-  it('Welcome renders hello world', () => {
-    const component = renderer.create(<Welcome />);
-    const json = component.toJSON();
-    expect(json).toMatchSnapshot();
-  });
+
+  describe('example sub describe', () => {
+    let component;
+    it('Welcome renders hello world', () => {
+      component = renderer.create(<Welcome />);
+      const json = component.toJSON();
+      expect(json).toMatchSnapshot();
+    });
+
+    it('should be accessible', () => {
+      axe.run(component, (err, { violations }) => {
+        expect(violations).toHaveLength(0);
+      });
+    })
+
+    it('should be 5', () => {
+      expect(5).toBe(5);
+    });
+  })
+
 });
